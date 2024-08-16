93f31d52028309adcc11080dbe947bc64809992a
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-08-16 12:02:30 -0300

    chore: Added basic task crud unit tests.

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import type { Task } from "./task.interface";
 import { TasksService } from "./tasks.service";
 
 describe("TasksService", () => {
@@ -14,5 +15,59 @@ describe("TasksService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should create a task", () => {
+    const taskTitle = "Task 1";
+    const expectedTask = {
+      title: taskTitle,
+      status: "open",
+    };
+
+    const result = service.createTask(taskTitle);
+
+    expect(result).toEqual({ id: expect.any(String), ...expectedTask });
+  });
+
+  it("should return all tasks", () => {
+    const expectedTasks = [
+      {
+        id: "1",
+        title: "Task 1",
+        status: "open",
+      },
+      {
+        id: "2",
+        title: "Task 2",
+        status: "open",
+      },
+      {
+        id: "3",
+        title: "Task 3",
+        status: "open",
+      },
+    ];
+
+    const result = service.readTasks();
+
+    expect(result).toEqual(expectedTasks);
+  });
+
+  it("should update a task", () => {
+    const taskId = "2";
+    const expectedUpdatedTask: Omit<Task, "id"> = {
+      title: "Task Bla",
+      status: "done",
+    };
+
+    const result = service.updateTask(taskId, expectedUpdatedTask);
+
+    expect(result).toEqual({ id: taskId, ...expectedUpdatedTask });
+  });
+
+  it("should delete a task", () => {
+    const result = service.deleteTask("2");
+
+    expect(result).toBe(undefined);
   });
 });
