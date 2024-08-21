176cef8ddcb9d590b33538be75eabbcd25771d5c
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-08-21 17:13:56 -0300

    feat: Validate app.config on startup.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,49 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  database: {
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-  },
-});
+import { plainToInstance, Type } from "class-transformer";
+import {
+  IsFQDN,
+  IsPort,
+  IsString,
+  IsStrongPassword,
+  validateSync,
+} from "class-validator";
+
+class DbConfig {
+  @IsFQDN() host: string;
+  @IsPort() port: string;
+  @IsString() database: string;
+  @IsString() username: string;
+  @IsStrongPassword() password: string;
+}
+
+class AppConfig {
+  @IsPort() port: string;
+  @Type(() => DbConfig) database: DbConfig;
+}
+
+const validateConfig = (config: Record<string, unknown>) => {
+  const validatedConfig = plainToInstance(AppConfig, config);
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+};
+
+export default () => {
+  const config = {
+    port: process.env.PORT || "3000",
+    database: {
+      host: process.env.DB_HOST || "localhost",
+      port: process.env.DB_PORT || "5432",
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+  };
+
+  return validateConfig(config);
+};
