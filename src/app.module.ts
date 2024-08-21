3630543129fcbf99e0ada53001d5c2c4e814f34b
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-08-21 12:43:52 -0300

    feat: Included configuration module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import appConfig from "./app.config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "db",
-      port: 5432,
-      username: "rampup",
-      password: "password",
-      database: "rampup",
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("database.host"),
+        port: configService.get("database.port"),
+        username: configService.get("database.username"),
+        password: configService.get("database.password"),
+        database: configService.get("database.name"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     TasksModule,
   ],
