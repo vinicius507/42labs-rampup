da79bee1ad1dfe7e32977dedaa364aa21c24733f
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-08-21 16:42:05 -0300

    feat: Added class validation to message requests.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { ValidationPipe, HttpStatus } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
 
   await app.listen(configService.get("port"));
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,6 +1,13 @@
-import { TaskStatus } from "../task.interface";
+import { IsEnum, IsString, Length } from "class-validator";
+import { TaskStatus, taskStatuses } from "../task.interface";
 
 export class UpdateTaskDTO {
+  @IsString()
+  @Length(3, 100)
   title: string;
+
+  @IsEnum(taskStatuses, {
+    message: `Status must be one of these values: ${taskStatuses.join(", ")}`,
+  })
   status: TaskStatus;
 }
